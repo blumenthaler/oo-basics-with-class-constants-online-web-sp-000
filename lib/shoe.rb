6fa1c3a421659da01603161b9656f94a26f0856e
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-01 17:57:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS.each do |brand|
+      if !BRANDS.include?(brand)
+        BRANDS << brand
+      end
+    end
   end
 
   def cobble
